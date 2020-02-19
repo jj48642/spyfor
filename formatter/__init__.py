631c2d9870f1e08145b37dfabb5a2c9884777860
jj48642 <ande1941@msu.edu>
2020-02-19 11:21:50 -0500

    Attempting to make the package installable from git

---
+++ formatter/__init__.py
@@ -0,0 +1,2 @@
+from .Format_Regression_Output import PrintRegressions
+from .Stata_Reg_Obj import res_obj
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spyfor',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Stata Regression Formatter',
+    long_description=open('README.md').read(),
+    install_requires=['pandas', 'copy', 'sfi', 're'],
+    url='https://github.com/jj48642/spyfor',
+    author='James J Anderson',
+    author_email='jj48642@gmail.com'
+)
