e5aba80436a3e6409f9c54f041deefec3d4fdb65
jj48642 <ande1941@msu.edu>
2020-02-20 08:56:30 -0500

    Added code to ensure that all variables used in parameters are identified within the reg_object.  This code will treat any unspecified variables as control variables for purposes of presentation.

--- formatter/Reg_Obj.py
+++ formatter/Reg_Obj.py
@@ -10,9 +10,21 @@ class RegObject:
         # This avoids errors in the formatter to make sure that there is no overlap of variables of
         # interest and control variables
         self.controls = []
-        for item in list(dict.fromkeys([x.lower() for x in  controls])):
+        for item in list(dict.fromkeys([x.lower() for x in controls])):
             if item not in self.variables_of_interest:
                 self.controls.append(item)
+
+        # creating a unique list of all parameters used in all of the specifications that are grouped together in case
+        # they are not specified as interest or control variables.  I will treat all variables not specified as a
+        # variable of interest as a control variable for presentation purposes.
+        params = []
+        for output in self.res_list:
+            params.append(output.param)
+        params = list(set(params))
+
+        for var in params:
+            if var not in self.variables_of_interest and var not in self.controls:
+                self.controls.append(var)
 
     def print_res(self, file_dir):
         with open(file_dir, 'w') as f:
